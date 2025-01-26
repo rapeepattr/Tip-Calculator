6cd656a22ebbd778029beb5127aaf98eae19d056
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-01-27 00:40:16 +0700

    Add calculate function

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ const billInput = document.getElementById('bill')
 billInput.addEventListener('change', calculateTip)
 
 function calculateTip() {
-    console.log('calculate tip')
+    let bill = parseFloat(billInput.value)
+    let tipPercent = document.getElementById('tip').value
+    let noOfPeople = document.getElementById('no-of-people').value
+    billInput.value = bill.toFixed(2)
+
+    let totalTip = parseFloat((bill * (tipPercent / 100)).toFixed(2))
+    let total = parseFloat((bill + totalTip).toFixed(2))
+    let tipPerPerson = (totalTip / noOfPeople).toFixed(2)
+    let totalPerPerson = (total / noOfPeople).toFixed(2)
+
+    document.getElementById('tip-amount').innerText = `$ ${totalTip}`
+    document.getElementById('total-amount').innerText = `$ ${total}`
+    document.getElementById('tip-percent').innerText = `${tipPercent}%`
+    document.getElementById('split-num').innerText = `${noOfPeople}`
+    document.getElementById('tip-per-person').innerText = `$${tipPerPerson}`
+    document.getElementById('total-per-person').innerText = `$${totalPerPerson}`
 }
